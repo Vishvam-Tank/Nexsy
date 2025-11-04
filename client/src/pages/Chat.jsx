a0aca29e685eef4012e78261922d4b99a99e65a8
Vishvam -23IT213 <vishvamtank@gmail.com>
2025-11-04 18:56:59 +0530

    Fix CSS import paths

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -4,7 +4,7 @@ import io from "socket.io-client";
 import axios from "axios";
 import { v4 as uuidv4 } from "uuid";
 import { API_URL, SOCKET_URL } from "../config";
-import "./chat.css";
+import "../chat.css";
 
 const socket = io(SOCKET_URL, { autoConnect: false });
 
