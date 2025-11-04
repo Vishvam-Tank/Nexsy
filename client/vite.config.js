fe16227968da4a1e156e7d415117f8214e8c7b77
Vishvam -23IT213 <vishvamtank@gmail.com>
2025-11-04 20:03:05 +0530

    Fix Vercel build: remove PostCSS config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -10,14 +11,6 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  },
-  define: {
-    'process.env': {}
+    emptyOutDir: true
   }
 })
